4d7974f8047e8a47bf73fedd43a9d9fcc0672008
Osku Väyrynen <osku27@hotmail.com>
2023-12-21 00:12:30 +0200

    updated managelogs.py and warcraftlogs.py.

--- commands/managelogs.py
+++ commands/managelogs.py
@@ -3,6 +3,8 @@ import hikari
 import lightbulb
 import json
 from utils import read_json_file
+import random
+from .warcraftlogs import initialize_log_checks
 
 
 def manage_logs_command(bot):
@@ -11,6 +13,56 @@ def manage_logs_command(bot):
     @lightbulb.implements(lightbulb.SlashCommandGroup)
     async def manage_logs(ctx):
         pass
+
+    # Subcommand for adding guilds
+    @manage_logs.child
+    @lightbulb.option("channels", "Comma-separated list of channel names", type=str, autocomplete=True)
+    @lightbulb.option("region", "The region of the guild", type=str, autocomplete=True)
+    @lightbulb.option("realm", "The realm of the guild", type=str)
+    @lightbulb.option("guild_name", "The name of the guild", type=str)
+    @lightbulb.command("add_guild", "Add a guild")
+    @lightbulb.implements(lightbulb.SlashSubCommand)
+    async def add_guild(ctx):
+        guild_name = ctx.options.guild_name
+        region = ctx.options.region
+        realm = ctx.options.realm
+        channels_input = ctx.options.channels
+
+        url = f"https://www.warcraftlogs.com:443/v1/reports/guild/{guild_name}/{realm}/{region}"
+
+        # Generate a random color
+        random_color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+
+        channels_list = [channel.strip() for channel in channels_input.split(',')]
+
+        guild_data = {
+            "url": url,
+            "color": random_color,
+            "filename": f"previouslogsid_{guild_name}.txt",
+            "channels": channels_list
+        }
+
+        # Read the current config
+        config = read_json_file('config.json')
+
+        # Ensure 'log_sources' key exists and it's a dictionary
+        if 'log_sources' not in config or not isinstance(config['log_sources'], dict):
+            config['log_sources'] = {}
+
+        # Add the guild data under 'log_sources'
+        config['log_sources'][guild_name] = guild_data
+
+        # Write back to config
+        with open('config.json', 'w') as file:
+            json.dump(config, file, indent=4)
+
+        # After updating config, reinitialize log checks
+        await initialize_log_checks(ctx.bot)
+
+        channels_str = ", ".join(channels_list)
+        response_message = f"Added guild '{guild_name}' in realm '{realm}' in region '{region}' to channels: {channels_str} under log sources."
+        await ctx.respond(response_message)
+        print(response_message)  # Debugging output
 
     @manage_logs.child
     @lightbulb.option("url", "The URL of the log source", type=str)
@@ -39,6 +91,9 @@ def manage_logs_command(bot):
         # Write back to config
         with open('config.json', 'w') as file:
             json.dump(config, file, indent=4)
+
+        # After updating config, reinitialize log checks
+        await initialize_log_checks(ctx.bot)
 
         response_message = f"Added log source '{log_source_name}' with filename '{filename}'."
         await ctx.respond(response_message)
@@ -81,6 +136,36 @@ def manage_logs_command(bot):
         # Find matches based on user input
         input_str = opt.value.lower()
         matches = [name for name in log_sources if input_str in name.lower()]
+
+        # Create a list of autocomplete choices
+        choices = [hikari.CommandChoice(name=name, value=name) for name in matches]
+
+        # Respond with the matches
+        return choices
+
+    @add_guild.autocomplete("region")
+    async def region_autocomplete(
+            opt: hikari.AutocompleteInteractionOption,
+            inter: hikari.AutocompleteInteraction
+    ) -> Union[
+        str, Sequence[str], hikari.api.AutocompleteChoiceBuilder, Sequence[hikari.api.AutocompleteChoiceBuilder]]:
+        regions = ["us", "eu", "kr", "tw", "cn"]
+        choices = [hikari.CommandChoice(name=region, value=region) for region in regions]
+        return choices
+
+    @add_guild.autocomplete("channels")
+    async def channels_autocomplete(
+            opt: hikari.AutocompleteInteractionOption,
+            inter: hikari.AutocompleteInteraction
+    ) -> Union[
+        str, Sequence[str], hikari.api.AutocompleteChoiceBuilder, Sequence[hikari.api.AutocompleteChoiceBuilder]]:
+        config = read_json_file('config.json')
+        channel_ids = config.get('channel_ids', {})
+        channel_names = list(channel_ids.keys())
+
+        # Find matches based on user input
+        input_str = opt.value.lower()
+        matches = [name for name in channel_names if input_str in name.lower()]
 
         # Create a list of autocomplete choices
         choices = [hikari.CommandChoice(name=name, value=name) for name in matches]
