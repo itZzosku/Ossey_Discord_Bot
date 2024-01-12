ea25e80f6dda321fa1284ff188ecda3acc0f59ad
Osku Väyrynen <osku27@hotmail.com>
2024-01-12 07:59:22 +0200

    updated hex to int

--- utils.py
+++ utils.py
@@ -6,13 +6,15 @@ def read_json_file(filename):
         return json.load(file)
 
 
-def hex_to_int(hex_color):
-    return int(hex_color.lstrip('#'), 16)
+def hex_to_int(hex_color: str) -> int:
+    hex_color = hex_color.lstrip('#')  # Remove '#' if present
+    return int(hex_color, 16)
 
 
 def get_discord_token():
     tokens = read_json_file('token.json')
     return tokens.get('discord_token', None)  # Provide a default value if necessary
+
 
 def get_influxdb2_token():
     tokens = read_json_file('token.json')
